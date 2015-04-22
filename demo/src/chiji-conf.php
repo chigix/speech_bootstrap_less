5546a0562c52e943af5d27a6966b485fcaf53c4a
Richard Lea <chigix@zoho.com>
2015-04-23 02:18:17 +0800

    new: usr: demo project.
    Signed-off-by: Richard Lea <chigix@zoho.com>

---
+++ demo/src/chiji-conf.php
@@ -0,0 +1,20 @@
+<?php
+
+class Configuration extends \Chigi\Chiji\Project\ProjectConfig {
+
+    public function getProjectName() {
+        return "speech_demo";
+    }
+
+    public function getRoadMap() {
+        $road_map = new \Chigi\Chiji\Collection\RoadMap();
+        $road_map->append(new \Chigi\Chiji\Project\LessRoad("LESS", new \Chigi\Component\IO\File("../../bower_components", $this->getProjectRootPath()), new \Chigi\Component\IO\File("../dist/vendor/", $this->getProjectRootPath())));
+        $road_map->append(new \Chigi\Chiji\Project\LessRoad("LESS", new \Chigi\Component\IO\File("./styles", $this->getProjectRootPath()), new \Chigi\Component\IO\File("../dist/css", $this->getProjectRootPath())));
+        $road_map->append(new \Chigi\Chiji\Project\SourceRoad("ROOT", new \Chigi\Component\IO\File($this->getProjectRootPath()), new \Chigi\Component\IO\File("../dist", $this->getProjectRootPath())));
+        return $road_map;
+    }
+
+}
+
+return new Configuration();
+?>
