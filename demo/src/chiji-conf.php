5969e605a916b5b37e4da974379a7c326af741ee
Richard Lea <chigix@zoho.com>
2015-04-23 16:08:02 +0800

    chg: usr: demo source.
    Signed-off-by: Richard Lea <chigix@zoho.com>

--- demo/src/chiji-conf.php
+++ demo/src/chiji-conf.php
@@ -8,7 +8,8 @@ class Configuration extends \Chigi\Chiji\Project\ProjectConfig {
 
     public function getRoadMap() {
         $road_map = new \Chigi\Chiji\Collection\RoadMap();
-        $road_map->append(new \Chigi\Chiji\Project\LessRoad("LESS", new \Chigi\Component\IO\File("../../bower_components", $this->getProjectRootPath()), new \Chigi\Component\IO\File("../dist/vendor/", $this->getProjectRootPath())));
+        $road_map->append(new \Chigi\Chiji\Project\LessRoad("VENDOR_LESS", new \Chigi\Component\IO\File("../../bower_components", $this->getProjectRootPath()), new \Chigi\Component\IO\File("../dist/vendor", $this->getProjectRootPath())));
+        $road_map->append(new \Chigi\Chiji\Project\SourceRoad("VENDOR_RC", new \Chigi\Component\IO\File("../../bower_components", $this->getProjectRootPath()), new \Chigi\Component\IO\File("../dist/vendor", $this->getProjectRootPath())));
         $road_map->append(new \Chigi\Chiji\Project\LessRoad("LESS", new \Chigi\Component\IO\File("./styles", $this->getProjectRootPath()), new \Chigi\Component\IO\File("../dist/css", $this->getProjectRootPath())));
         $road_map->append(new \Chigi\Chiji\Project\SourceRoad("ROOT", new \Chigi\Component\IO\File($this->getProjectRootPath()), new \Chigi\Component\IO\File("../dist", $this->getProjectRootPath())));
         return $road_map;
